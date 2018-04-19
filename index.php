8e1a09153bf0660ee8013cc623508f43e114dd87
Jonathan Simpson <jsimpson@jdsnetwork.com>
2018-04-19 13:46:37 -0400

    More adaptable logos

--- index.php
+++ index.php
@@ -18,34 +18,32 @@ if (array_key_exists("state", $_GET) and $_GET["state"] == "on" or $_GET["state"
 }
 $url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $output = shell_exec("expect /var/tmp/power.expect ".$ip." ".$user." ".$password." ".$port." ".$state." | tail -n 10 | head -n 8 | cut -c 7,34-39");
-#foreach(preg_split("/((\r?\n)|(\r\n?))/", $output) as $line){
-#	$stat = explode(" ", $line, 2);
-#	if ($stat[0]) {echo $stat[0]." currently ".$stat[1]." <a href='".$url."?port=".$stat[0]."&state=on'>ON</a> <a href='".$url."?port=".$stat[0]."&state=off'>OFF</a><br>";}
-#}
 $outletstats = preg_split("/((\r?\n)|(\r\n?))/", $output);
 ?>
 <table style="width:100%;border: 1px solid black;">
   <tr>
     <th>Function</th>
-    <th>Port</th> 
     <th>Status</th>
     <th>Control</th>
   </tr>
   <tr>
     <td>Downstairs Exhaust Fan</td>
-    <td>1</td>
     <td><?php echo explode(" ",$outletstats[0])[1];?></td>
-    <td><?php echo "<a href='".$url."?state=on&port=1'><img src='./img/button_on_nact.png'></a><a href='".$url."?state=off&port=1'><img src='./img/button_off_nact.png'></a>";?></td>
+    <?php if (explode(" ",$outletstats[0])[1] == "On") {
+    echo "<td><a href='".$url."?state=on&port=1'><img src='./img/button_on_act.png'></a><a href='".$url."?state=off&port=1'><img src='./img/button_off_nact.png'></a></td>";
+    } elseif (explode(" ",$outletstats[0])[1] == "Off") {
+    echo "<td><a href='".$url."?state=on&port=1'><img src='./img/button_on_nact.png'></a><a href='".$url."?state=off&port=1'><img src='./img/button_off_act.png'></a></td>";}?>
   </tr>
   <tr>
-    <td>Deck Floodlight</td>
-    <td>2</td> 
+    <td>Deck Floodlights</td>
     <td><?php echo explode(" ",$outletstats[1])[1];?></td>
-    <td><?php echo "<a href='".$url."?state=on&port=2'><img src='./img/button_on_nact.png'></a><a href='".$url."?state=off&port=2'><img src='./img/button_off_nact.png'></a>";?></td>
+    <?php if (explode(" ",$outletstats[1])[1] == "On") {
+    echo "<td><a href='".$url."?state=on&port=2'><img src='./img/button_on_act.png'></a><a href='".$url."?state=off&port=2'><img src='./img/button_off_nact.png'></a></td>";
+    } elseif (explode(" ",$outletstats[1])[1] == "Off") {
+    echo "<td><a href='".$url."?state=on&port=2'><img src='./img/button_on_nact.png'></a><a href='".$url."?state=off&port=2'><img src='./img/button_off_act.png'></a></td>";}?>
   </tr>
   <tr>
     <td>Basement Lights</td>
-    <td>3</td>
     <td><?php echo explode(" ",$outletstats[2])[1];?></td>
     <td><?php echo "<a href='".$url."?state=on&port=3'><img src='./img/button_on_nact.png'></a><a href='".$url."?state=off&port=3'><img src='./img/button_off_nact.png'></a>";?></td>
   </tr>
